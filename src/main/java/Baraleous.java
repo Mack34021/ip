4a9831efbc24df8553670c6ae5a0c3e7a1332759
2025MEMES\jamie <jamiewillis200246@gmail.com>
2025-08-26 19:16:04 +0800

    Rename Duke to Baraleous. Track Baraleous.java

---
+++ src/main/java/Baraleous.java
@@ -0,0 +1,12 @@
+public class Baraleous {
+    public static void main(String[] args) {
+        String logo = "__________________________\n" +
+                "Hello! I'm Baraleous XIV!\n" +
+                "What can I do for you today?\n" +
+                "____________________________\n" +
+                "Goodbye!\n" +
+                "____________________________\n";
+        System.out.println("\n" + logo);
+    }
+}
+
