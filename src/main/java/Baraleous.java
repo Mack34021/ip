9760de6de0ed53cf48375e2150a9643c2df8defe
2025MEMES\jamie <jamiewillis200246@gmail.com>
2025-09-02 16:36:47 +0800

    Add echo function

--- src/main/java/Baraleous.java
+++ src/main/java/Baraleous.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Baraleous {
     public static void main(String[] args) {
-        String logo = "__________________________\n" +
-                "Hello! I'm Baraleous XIV!\n" +
-                "What can I do for you today?\n" +
-                "____________________________\n" +
-                "Goodbye!\n" +
-                "____________________________\n";
-        System.out.println("\n" + logo);
+        String logo = "Hello! I'm Baraleous XIV!\n" +
+                "  What can I do for you today?";
+        printMessage(logo);
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String userInput = scanner.nextLine();
+            if(userInput.equals("bye")){
+                printMessage("Goodbye!");
+                break;
+            }else{
+                printMessage(userInput);
+            }
+        }
+    }
+
+    /**
+     * Prints to terminal in a nice formatted manner.
+     * @param message String to be printed to the user
+     * todo: manage messages with '/n' characters - replace with '/n  '.
+     */
+    private static void printMessage(String message){
+        System.out.println("  _________Baraleous__________");
+        System.out.println("  " + message);
+        System.out.println("  ____________User____________");
     }
 }
+
 
